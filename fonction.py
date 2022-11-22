b86dd492ec04010b9d620ad2b725e8f2445c61ce
hidog123 <52358194+hidog123@users.noreply.github.com>
2022-11-22 23:18:18 +0100

    Create fonction.py

---
+++ fonction.py
@@ -0,0 +1,6 @@
+#Q4
+def image_noire(h,l):                                                                
+ return  np.zeros((h,l))
+#Q5
+def image_noire(h,l):                                                                
+ return  np.ones((h,l))
