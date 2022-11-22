b33e0a30e29e3f8a63ef72a1b22b21c3c3c4e9b1
hidog123 <52358194+hidog123@users.noreply.github.com>
2022-11-22 01:22:22 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,7 +1,17 @@
+import matplotlib.pyplot as plt
 from art import text2art, tprint, art 
 from colorama import Fore
+import numpy as np
+def image_noire(h,l):                                                                
+ return  np.zeros((h,l))
+
+def AfficherImg(img):
+ plt.axis("off")
+ plt.imshow(img, interpolation="nearest")
+ plt.show()
+
 print(text2art("|TP Python|"))
-k=k_=k__=0
+x_=y_=k=k_=k__=0
 while (k !=1 and k!=2 and k!=3 and k!=4):
  print(Fore.RED + '1)Partie 1')
  print(Fore.RED + '2)Partie 2')
@@ -20,6 +30,13 @@ while (k !=1 and k!=2 and k!=3 and k!=4):
      if k_==1 :
       while(k__!=1 and k__!=2):
         print(Fore.RED + '1)Print source code')
-        print(Fore.RED + '1)Execute')
+        print(Fore.RED + '2)Execute')
         print(Fore.GREEN + 'chosse correcte value:')
         k__=int(input())
+        if k__==1:
+          print(Fore.YELLOW +"import numpy as np\ndef image_noire(h,l):\n return  np.zeros((h,l))")
+        else :
+          x_:int(input("rentrer hauteur de photo"))
+          y_:int(input("rentrer largeur de photo"))
+          immg=image_noire(x_,y_)
+          AfficherImg(immg)
